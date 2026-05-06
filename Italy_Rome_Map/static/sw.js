87912a36c20ed5e6eb6ef5519a16a3623e2fe104
Samuel Karlsson <samuel.karlsson00@gmail.com>
2026-05-06 17:46:37 +0200

    Test on offline

--- Italy_Rome_Map/static/sw.js
+++ Italy_Rome_Map/static/sw.js
@@ -1,18 +1,21 @@
-const CACHE = 'roma-tavola-v2';
+const CACHE = 'roma-tavola-v3';
 const ASSETS = [
   '/',
   '/static/css/style.css',
   '/static/js/map.js',
+  '/static/manifest.json',
   '/api/restaurants',
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', e => {
+  self.clients.claim();
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
@@ -24,7 +27,6 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(response => {
-        // Cache successful responses
         const clone = response.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return response;
